75ce5511964638bdbef5a22c725fdcbdfac9f01d
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-10 17:45:25 -0500

    reorg defaults

--- store.rb
+++ store.rb
@@ -7,24 +7,24 @@ class Store
   attr_reader :inventory, :menu
 
   DEFAULT_INVENTORY =
-    { Ingredient::COFFEE => 10,
-      Ingredient::DECAF_COFEE => 10,
-      Ingredient::SUGAR => 10,
+    { Ingredient::COCOA => 10,
+      Ingredient::COFFEE => 10,
       Ingredient::CREAM => 10,
-      Ingredient::STEAMED_MILK => 10,
-      Ingredient::FOAMED_MILK => 10,
+      Ingredient::DECAF_COFEE => 10,
       Ingredient::ESPRESSO => 10,
-      Ingredient::COCOA => 10,
+      Ingredient::FOAMED_MILK => 10,
+      Ingredient::STEAMED_MILK => 10,
+      Ingredient::SUGAR => 10,
       Ingredient::WHIPPED_CREAM  => 10
     }
 
     DEFAULT_MENU =
-      [ Drink.new({name: "coffee",ingredients: {"coffee" => 1,"sugar" => 1, "cream" => 1}}),
-      Drink.new({name: "decaf coffee",ingredients: {"decaf coffee" => 3,"sugar" => 1, "cream" => 1}}),
-      Drink.new({name: "caffe latte",ingredients: {"espresso" => 2,"steamed milk" => 1}}),
-      Drink.new({name: "cafe americano",ingredients: {"espresso" => 3}}),
-      Drink.new({name: "caffe mocha",ingredients: {"espresso" => 1,"steamed milk" => 1, "cocoa" => 1, "whipped cream" => 1}}),
-      Drink.new({name: "cappuccino",ingredients: {"espresso" => 2,"steamed milk" => 1, "foamed milk" => 1}})
+      [ Drink.new({name: "cafe americano",ingredients: {Ingredient::ESPRESSO => 3}}),
+      Drink.new({name: "caffe latte",ingredients: {Ingredient::ESPRESSO => 2, Ingredient::STEAMED_MILK => 1}}),
+      Drink.new({name: "caffe mocha",ingredients: {Ingredient::ESPRESSO => 1,Ingredient::STEAMED_MILK => 1, Ingredient::COCOA => 1, Ingredient::WHIPPED_CREAM => 1}}),
+      Drink.new({name: "cappuccino",ingredients: {Ingredient::ESPRESSO => 2,Ingredient::STEAMED_MILK => 1, Ingredient::FOAMED_MILK => 1}})
+      Drink.new({name: "coffee",ingredients: {Ingredient::COFFEE => 1,Ingredient::SUGAR => 1, Ingredient::CREAM => 1}}),
+      Drink.new({name: "decaf coffee",ingredients: {Ingredient::DECAF_COFFEE => 3,Ingredient::SUGAR => 1, Ingredient::CREAM => 1}}),
       ]
 
   def initialize(args = {})
