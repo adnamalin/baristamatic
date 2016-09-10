780685a5623db84396ce22da463e0ccbc6c3a200
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-10 16:57:31 -0500

    start teting inventory

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,17 +1,28 @@
 require_relative '../store'
+require_relative '../ingredient'
 
 RSpec.describe Store do
 
-  let(:store) { Store.new(inventory: {}) }
+  let(:store) { Store.new()}
 
   describe 'attributes' do
     it 'has an inventory' do
-      expect(account.name).to eq 'Backstop Solutions'
+      expect(store.inventory).to be_a Hash
+    end
+
+    it 'has menu' do
+      expect(store.menu).to be_a Hash
+    end
+  end
+
+  describe 'default attributes' do
+    it 'has an inventory with default stock if not provided' do
+      expect(store.inventory).to eq Store::DEFAULT_INVENTORY
     end
 
     it 'has menu with drinks
     ' do
-      expect(account.id).to eq "1234"
+      expect(store.menu).to eq Store::DEFAULT_MENU
     end
   end
 
