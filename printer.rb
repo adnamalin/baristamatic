bf193bc4236c8348260d4d5bf0c184380d93eb9b
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-10 20:08:09 -0500

    format cost output

--- printer.rb
+++ printer.rb
@@ -11,9 +11,16 @@ class Printer
     counter = 1
     puts "Menu:"
     store.menu.each do |drink|
-      puts "#{counter}, #{drink.name.capitalize}, $#{drink.cost}, #{store.in_stock?(drink)} "
+      puts "#{counter}, #{drink.name.capitalize}, $#{cost_format(drink.cost)}, #{store.in_stock?(drink)} "
       counter += 1
     end
   end
 
+  def cost_format(cost)
+    if cost.to_s.length == 3
+      cost.to_s + "0"
+    else
+      cost.to_s
+    end
+  end
 end
